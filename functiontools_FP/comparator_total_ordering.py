cca7721d41ff37a3996efbe6952da5f9e77d17a9
ayush884 <ayush@whatsbusy.com>
2020-06-18 14:10:16 -0700

    added comparator total_ordering

---
+++ functiontools_FP/comparator_total_ordering.py
@@ -0,0 +1,36 @@
+# Using Magic functions for comparison
+# __eq__, __lt__, __gt__, __le__, __le__
+
+class Car:      # Comparator functions
+    def __init__(self, model, mileage):
+        self.model = model
+        self.mileage = mileage
+
+    def __lt__(self, car2):
+        return self.mileage < car2.mileage
+
+    def __gt__(self, car2):
+        return self.mileage > car2.mileage
+        
+    def __le__(self, car2):
+        return self.mileage <= car2.mileage
+
+    def __ge__(self, car2):
+        return self.mileage >= car2.mileage
+    
+    def __eq__(self, car2):
+        return self.mileage == car2.mileage
+
+    def __ne__(self, car2):
+        return self.mileage != car2.mileage
+
+ # Comparator
+car1 = Car("Audi",10)
+car2 = Car("Honda",20)
+
+print('car1 ("Audi",10) == car2 ("Honda",20): ', car1==car2)
+print('car1 ("Audi",10) != car2 ("Honda",20): ', car1!=car2)
+print('car1 ("Audi",10) < car2 ("Honda",20): ', car1<car2)
+print('car1 ("Audi",10) <= car2 ("Honda",20): ', car1<=car2)
+print('car1 ("Audi",10) > car2 ("Honda",20): ', car1>car2)
+print('car1 ("Audi",10) >= car2 ("Honda",20): ', car1>=car2)
--- functiontools_FP/reduce.py
+++ functiontools_FP/reduce.py
@@ -18,3 +18,8 @@ students = [
 
 sum = reduce(lambda x,y: x + y["marks"], students, 0)
 print('sum: ', sum)
+
+# reduce is not easy to use here, so we take max
+# max_score2 = reduce(lambda x,y: x if x["marks"] > y["marks"] else y, students, 0)
+max_score_student = max(students, key= lambda x: x["marks"])
+print('max_score_student: ', max_score_student)
