a206b66c0e1bca4f4698d86faded15d3b7f58f79
ayush884 <ayush@whatsbusy.com>
2020-07-14 01:09:01 -0700

    enumerate in zip

--- built_in_functions/zip.py
+++ built_in_functions/zip.py
@@ -14,4 +14,14 @@ print('\nset(zipped): ', set(zip(names, companies)))
 
 print('\ntuple(zipped): ', tuple(zip(names, companies)))
 
-print('\ndict(zipped): ', dict(zip(names, companies)))
+print('\ndict(zipped): ', dict(zip(names, companies)), "\n")
+
+# Iterable
+for zip_val in zipped:
+    print('zip_val: ', zip_val)
+
+print("\n")
+
+# enumerate
+for idx, zip_val in enumerate(zip(names, companies)):
+    print('idx, zip_val: ', idx, zip_val)
